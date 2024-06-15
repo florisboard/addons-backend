da2b690fd8361f7edb416b73c6769a4e532a1e81
4H1R <ghostrockz3d@gmail.com>
2024-06-15 20:58:28 +0330

    update packages

--- _ide_helper.php
+++ _ide_helper.php
@@ -5,7 +5,7 @@
 
 /**
  * A helper file for Laravel, to provide autocomplete information to your IDE
- * Generated for Laravel 10.48.11.
+ * Generated for Laravel 10.48.12.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
