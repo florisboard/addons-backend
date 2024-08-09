9219bd5226634875c402cca3b4de49e121cd661b
4H1R <ghostrockz3d@gmail.com>
2024-08-10 00:53:11 +0330

    add public private s3

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DISK', 'local'),
+    'default' => env('FILESYSTEM_DISK', 's3'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,20 +39,34 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
 
-        's3' => [
+        's3_private' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'key' => env('PRIVATE_AWS_ACCESS_KEY_ID'),
+            'secret' => env('PRIVATE_AWS_SECRET_ACCESS_KEY'),
+            'region' => env('PRIVATE_AWS_DEFAULT_REGION'),
+            'bucket' => env('PRIVATE_AWS_BUCKET'),
+            'url' => env('PRIVATE_AWS_URL'),
+            'endpoint' => env('PRIVATE_AWS_ENDPOINT'),
+            'visibility' => 'private',
+            'use_path_style_endpoint' => env('PRIVATE_AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+        ],
+
+        's3_public' => [
+            'driver' => 's3',
+            'key' => env('PUBLIC_AWS_ACCESS_KEY_ID'),
+            'secret' => env('PUBLIC_AWS_SECRET_ACCESS_KEY'),
+            'region' => env('PUBLIC_AWS_DEFAULT_REGION'),
+            'bucket' => env('PUBLIC_AWS_BUCKET'),
+            'url' => env('PUBLIC_AWS_URL'),
+            'endpoint' => env('PUBLIC_AWS_ENDPOINT'),
+            'visibility' => 'public',
+            'use_path_style_endpoint' => env('PUBLIC_AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
 
